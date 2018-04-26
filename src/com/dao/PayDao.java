99009157bbe1d928e80de27f32cb82d552588e8c
heyi <1378819010@qq.com>
2018-04-26 14:23:47 +0800

    heyi pay

---
+++ src/com/dao/PayDao.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import com.entity.UserEntity;
+/**
+ * @description：
+ * @author：heyi
+ * @date：2018/4/25 18:47
+ * @version：v1.0
+ */
+public class PayDao extends BaseDaoImpl<UserEntity>{
+
+}

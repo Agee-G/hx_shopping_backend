1c32b97ec7eb73d09a7255b36f2adbec903a449a
1239236430@qq.com <dear.1314>
2018-04-26 10:21:32 +0800

    update .gitignore55

--- README.md
+++
@@ -1,2 +0,0 @@
-# hx_shopping_backend
-2018专业设计 商城接口部分
--- src/com/dao/BackendDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-/**
- * @author 汤新苗-20155864
- * @Description:${todo}
- * @date 25/04/201808:20
- */
-public class BackendDao{
-}
